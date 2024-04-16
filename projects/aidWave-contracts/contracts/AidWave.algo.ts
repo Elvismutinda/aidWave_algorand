3adadc9bb582ba8ba250beb9c910997bb59a6bc1
Elvismutinda <elvocool@gmail.com>
2024-04-16 15:26:33 +0300

    Added methods for getting votes and proposals

--- projects/aidWave-contracts/contracts/AidWave.algo.ts
+++ projects/aidWave-contracts/contracts/AidWave.algo.ts
@@ -2,57 +2,25 @@ import { Contract } from '@algorandfoundation/tealscript';
 
 // eslint-disable-next-line no-unused-vars
 class AidWave extends Contract {
-  /**
-   * Calculates the sum of two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The sum of a and b
-   */
-  private getSum(a: uint64, b: uint64): uint64 {
-    return a + b;
+  proposal = GlobalStateKey<string>();
+
+  voteTotal = GlobalStateKey<uint64>();
+
+  // define a proposal
+  createApplication(proposal: string): void {
+    this.proposal.value = proposal;
   }
 
-  /**
-   * Calculates the difference between two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The difference between a and b.
-   */
-  private getDifference(a: uint64, b: uint64): uint64 {
-    return a >= b ? a - b : b - a;
+  vote(): void {
+    this.voteTotal.value = this.voteTotal.value + 1;
   }
 
-  /**
-   * A method that takes two numbers and does either addition or subtraction
-   *
-   * @param a The first uint64
-   * @param b The second uint64
-   * @param operation The operation to perform. Can be either 'sum' or 'difference'
-   *
-   * @returns The result of the operation
-   */
-  doMath(a: uint64, b: uint64, operation: string): uint64 {
-    let result: uint64;
-
-    if (operation === 'sum') {
-      result = this.getSum(a, b);
-    } else if (operation === 'difference') {
-      result = this.getDifference(a, b);
-    } else throw Error('Invalid operation');
-
-    return result;
+  // make it easy for voters to see what the proposal is
+  getProposal(): string {
+    return this.proposal.value;
   }
 
-  /**
-   * A demonstration method used in the AlgoKit fullstack template.
-   * Greets the user by name.
-   *
-   * @param name The name of the user to greet.
-   * @returns A greeting message to the user.
-   */
-  hello(name: string): string {
-    return 'Hello, ' + name;
+  getVotes(): uint64 {
+    return this.voteTotal.value;
   }
 }
