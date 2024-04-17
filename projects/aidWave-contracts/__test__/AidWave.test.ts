9aed0c80331b4b874607839eba1758d4e66b0781
Elvismutinda <elvocool@gmail.com>
2024-04-17 12:59:53 +0300

    Added tests for preventing a user from voting without registering

--- projects/aidWave-contracts/__test__/AidWave.test.ts
+++ projects/aidWave-contracts/__test__/AidWave.test.ts
@@ -12,12 +12,14 @@ let appClient: AidWaveClient;
 describe('AidWave', () => {
   beforeEach(fixture.beforeEach);
   const proposal = 'AidWave is a decentralized platform that connects donors with those in need.';
+  let algod: algosdk.Algodv2;
   let sender: algosdk.Account;
-  let registeredAsa: bigint;
+  let registeredASA: bigint;
 
   beforeAll(async () => {
     await fixture.beforeEach();
-    const { algod, testAccount, kmd } = fixture.context;
+    const { testAccount, kmd } = fixture.context;
+    algod = fixture.context.algod;
 
     sender = await algokit.getOrCreateKmdWalletAccount(
       {
@@ -38,7 +40,7 @@ describe('AidWave', () => {
     );
 
     await appClient.create.createApplication({ proposal });
-  });
+  }, 15_000);
 
   test('getProposal', async () => {
     const proposalFromMethod = await appClient.getProposal({});
@@ -72,25 +74,61 @@ describe('AidWave', () => {
       }
     );
 
-    registeredAsa = bootstrapResult.return!.valueOf();
+    registeredASA = bootstrapResult.return!.valueOf();
   });
 
-  test('getRegisteredAsa', async () => {
-    const proposalFromMethod = await appClient.getRegiseredAsa({});
-    expect(proposalFromMethod.return?.valueOf()).toBe(registeredAsa);
+  test('vote (Negatve)', async () => {
+    await expect(appClient.vote({ inFavor: true, registeredASA })).rejects.toThrow();
+  });
+
+  test('getRegisteredASA', async () => {
+    const registeredAsaFromMethod = await appClient.getRegiseredAsa({});
+    expect(registeredAsaFromMethod.return?.valueOf()).toBe(registeredASA);
+  });
+
+  test('register', async () => {
+    const registeredAsaOptInTxn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+      from: sender.addr,
+      to: sender.addr,
+      amount: 0,
+      suggestedParams: await algokit.getTransactionParams(undefined, algod),
+      assetIndex: Number(registeredASA),
+    });
+
+    await algokit.sendTransaction({ from: sender, transaction: registeredAsaOptInTxn }, algod);
+
+    await appClient.register(
+      { registeredASA },
+      {
+        sender,
+        sendParams: {
+          fee: algokit.microAlgos(3_000),
+        },
+      }
+    );
+
+    const registeredAsaTransferTxn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+      from: sender.addr,
+      to: sender.addr,
+      amount: 1,
+      assetIndex: Number(registeredASA),
+      suggestedParams: await algokit.getTransactionParams(undefined, algod),
+    });
+
+    await expect(
+      algokit.sendTransaction({ from: sender, transaction: registeredAsaTransferTxn }, algod)
+    ).rejects.toThrow();
   });
 
   test('vote & getVote', async () => {
-    await appClient.vote({ inFavor: true });
+    await appClient.vote({ inFavor: true, registeredASA }, { sender });
 
     const votesAfter = await appClient.getVotes({});
-
     expect(votesAfter.return?.valueOf()).toEqual([BigInt(1), BigInt(1)]);
 
-    await appClient.vote({ inFavor: false });
+    await appClient.vote({ inFavor: false, registeredASA }, { sender });
 
     const votesAfter2 = await appClient.getVotes({});
-
     expect(votesAfter2.return?.valueOf()).toEqual([BigInt(1), BigInt(2)]);
   });
 });
