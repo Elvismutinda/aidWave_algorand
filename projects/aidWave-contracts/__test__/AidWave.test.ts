fbd97ef3cd9cceb4fe31b172d2b3c5965ae5e036
Elvismutinda <elvocool@gmail.com>
2024-04-17 11:29:08 +0300

    Added tests for multiple voting from one address

--- projects/aidWave-contracts/__test__/AidWave.test.ts
+++ projects/aidWave-contracts/__test__/AidWave.test.ts
@@ -1,6 +1,7 @@
 import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
 import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
 import * as algokit from '@algorandfoundation/algokit-utils';
+import algosdk from 'algosdk';
 import { AidWaveClient } from '../contracts/clients/AidWaveClient';
 
 const fixture = algorandFixture();
@@ -11,10 +12,21 @@ let appClient: AidWaveClient;
 describe('AidWave', () => {
   beforeEach(fixture.beforeEach);
   const proposal = 'AidWave is a decentralized platform that connects donors with those in need.';
+  let sender: algosdk.Account;
+  let registeredAsa: bigint;
 
   beforeAll(async () => {
     await fixture.beforeEach();
-    const { algod, testAccount } = fixture.context;
+    const { algod, testAccount, kmd } = fixture.context;
+
+    sender = await algokit.getOrCreateKmdWalletAccount(
+      {
+        name: 'daoSender',
+        fundWith: algokit.algos(10),
+      },
+      algod,
+      kmd
+    );
 
     appClient = new AidWaveClient(
       {
@@ -31,6 +43,41 @@ describe('AidWave', () => {
   test('getProposal', async () => {
     const proposalFromMethod = await appClient.getProposal({});
     expect(proposalFromMethod.return?.valueOf()).toBe(proposal);
+  });
+
+  test('bootstrap, (Negative)', async () => {
+    await appClient.appClient.fundAppAccount(algokit.microAlgos(200_000));
+    // default fee per txn is 0.001 Algos or 1_000 uAlgos
+    // bootstrap sends 1 txn, so 2 txns total, thus, fee needs to be 2_000 uAlgos
+    await expect(
+      appClient.bootstrap(
+        {},
+        {
+          sender,
+          sendParams: {
+            fee: algokit.microAlgos(2_000),
+          },
+        }
+      )
+    ).rejects.toThrow();
+  });
+
+  test('bootstrap', async () => {
+    const bootstrapResult = await appClient.bootstrap(
+      {},
+      {
+        sendParams: {
+          fee: algokit.microAlgos(2_000),
+        },
+      }
+    );
+
+    registeredAsa = bootstrapResult.return!.valueOf();
+  });
+
+  test('getRegisteredAsa', async () => {
+    const proposalFromMethod = await appClient.getRegiseredAsa({});
+    expect(proposalFromMethod.return?.valueOf()).toBe(registeredAsa);
   });
 
   test('vote & getVote', async () => {
