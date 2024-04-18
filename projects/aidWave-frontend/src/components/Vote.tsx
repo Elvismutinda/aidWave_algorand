b3c35c56c335922c62e2d303c8c3c2eb86b11274
Elvismutinda <elvocool@gmail.com>
2024-04-18 11:23:33 +0300

    Component that handles voting

---
+++ projects/aidWave-frontend/src/components/Vote.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable no-console */
+import { ReactNode, useState } from 'react'
+import { AidWave, AidWaveClient } from '../contracts/AidWave'
+import { useWallet } from '@txnlab/use-wallet'
+
+type VoteArgs = AidWave['methods']['vote(bool,uint64)void']['argsObj']
+
+type Props = {
+  buttonClass: string
+  buttonLoadingNode?: ReactNode
+  buttonNode: ReactNode
+  typedClient: AidWaveClient
+  inFavor: VoteArgs['inFavor']
+  registeredASA: VoteArgs['registeredAsa']
+  setState: () => Promise<void>
+}
+
+const Vote = (props: Props) => {
+  const [loading, setLoading] = useState<boolean>(false)
+  const { activeAddress, signer } = useWallet()
+  const sender = { signer, addr: activeAddress! }
+
+  const callMethod = async () => {
+    setLoading(true)
+    console.log(`Calling vote`)
+    await props.typedClient.vote(
+      {
+        inFavor: props.inFavor,
+        registeredAsa: props.registeredASA,
+      },
+      { sender },
+    )
+    await props.setState()
+    setLoading(false)
+  }
+
+  return (
+    <button className={props.buttonClass} onClick={callMethod}>
+      {loading ? props.buttonLoadingNode || props.buttonNode : props.buttonNode}
+    </button>
+  )
+}
+
+export default Vote
